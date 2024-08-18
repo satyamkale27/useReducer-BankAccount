0736b58cf92ea211ca71f2d00670fd8a5c0333f5
satyam <satyamkale271@gmail.com>
2024-08-18 13:49:48 +0530

    starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: X</p>
+      <p>Loan: X</p>
+
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Open account
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Close account
+        </button>
+      </p>
     </div>
   );
 }
-
-export default App;
