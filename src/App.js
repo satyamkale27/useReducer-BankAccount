279251652bca978c7faef584b375194aa39abf4a
satyam <satyamkale271@gmail.com>
2024-08-18 14:14:46 +0530

    balance case add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useReducer } from "react";
 const initialState = {
   balance: 0,
   loan: 0,
   isActive: false,
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, balance: 500 };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
+
 export default function App() {
+  const [{ balance }, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
-      <p>Balance: X</p>
+      <p>Balance: {balance}</p>
       <p>Loan: X</p>
 
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "openAccount" });
+          }}
+          disabled={false}
+        >
           Open account
         </button>
       </p>
